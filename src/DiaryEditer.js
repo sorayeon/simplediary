c9572baa30f93b451382459bab57df66ae216623
sorayeon <jihwan77@gmail.com>
2022-01-23 05:34:55 +0900

    React에서 DOM 조작하기 - useRef
    입력값에 대한 유효성 검사
    useRef 사용법
     (DOM 요소에 접근하여 focus 주는 방법)

--- src/DiaryEditer.js
+++ src/DiaryEditer.js
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import React, { useRef, useState } from "react"; 
 
 const DiaryEditer = () => {
+
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,7 +19,15 @@ const DiaryEditer = () => {
   }
 
   const handleSubmit = () => {
-    console.log(state);
+    if(state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if(state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
     alert("저장 성공");
   }
 
@@ -24,6 +36,7 @@ const DiaryEditer = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input 
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -31,6 +44,7 @@ const DiaryEditer = () => {
       </div>
       <div>
         <textarea 
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
