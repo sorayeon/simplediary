734cd6fe788107c81d7a4771d3b21ddd6a704b1c
sorayeon <jihwan77@gmail.com>
2022-01-23 04:45:14 +0900

    프로젝트 환경설정
    create-react-app 
    현재 프로젝트에서는 불필요한 테스트 소스 삭제
    기본화면 초기화 및 assets 삭제

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DiaryEditer from './DiaryEditer';
+
+function App() {
+  return (
+    <div className="App">
+      <DiaryEditer />
+    </div>
+  );
+}
+
+export default App;
