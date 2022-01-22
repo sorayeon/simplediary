99cbe26adbdc611b4650c112e8bfb7e9d306df58
sorayeon <jihwan77@gmail.com>
2022-01-23 06:21:01 +0900

    React에서 배열 사용하기 - 리스트 렌더링 (조회)
    1. 배열을 이용하여 React 에서 LIST 렌더링
    각각의 아이템을 개별적인 컴포넌트로 만들기
     Array.map((it)=><Componet key={it.id} {...it} />
    2. 리스트가 undefined 에러처리를 위한 defaultProps 적용
    3. milliseconds 를 다시 Date 로 변환 toLocaleString() 함수 이용
    4. List 와 Item CSS스타일적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
 import DiaryEditer from './DiaryEditer';
+import DiaryList from './DiaryList';
+import './App.css';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "소라연",
+    content: "안녕하세요 ^________^",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "안녕하세요 ^^",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "김철수",
+    content: "안녕하세요 ~~!!",
+    emotion: 1,
+    created_date: new Date().getTime()
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditer />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
