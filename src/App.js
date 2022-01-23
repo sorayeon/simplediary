ff1db885be0433cb837a3550edfa1cd6079c98d4
sorayeon <jihwan77@gmail.com>
2022-01-23 21:47:29 +0900

    React Lifecycle 및 API 호출하기
    1. useEffect 컴포넌트 생명주기
    - 컴포넌트 생성되면 콜백함수 실행 (Mount)
    useEffect(() => {}, []); 
    - 컴포넌트 변경(props, state ..)되면 콜백함수실행 (Update)
    useEffect(() => {});
    - 컴포넌트 Dependency Array 값이 변화하면 콜백함수실행
    useEffect(() => {}, [count]);
    - 컴포넌트가 소멸되면 리턴에 정의한 콜백함수 실행 (Unmount)
    useEffect(() => {
      return () = {}
    }, [count]);
    
    2. fetch : javascript api 호출 내장함수를 통해 호출
    API의 응답데이터를 일기 데이터의 초기값으로 이용

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import { useRef, useState } from 'react';
 import DiaryEditer from './DiaryEditer';
 import DiaryList from './DiaryList';
 import './App.scss';
+import { useEffect } from 'react/cjs/react.development';
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getDate = async () => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/1/comments',
+    ).then((res) => res.json());
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getDate();
+  }, []);
 
   // 등록
   const onCreate = (author, content, emotion) => {
