c25863436879ddf1b39acf264117b4462c8099a2
sorayeon <jihwan77@gmail.com>
2022-01-24 14:03:26 +0900

    복잡한 상태변화 로직 분리 - useReducer
    컴포넌트에서 상태변화 로직을 분리
    
    중요) dispatch는 useCallback를 사용하면서 dependency array를 걱정하지 않아도 좋다. 함수형 업데이트 신경쓰지 않아도 됨 (현재의 state를 리듀서 함수가 참조하므로)
    
    1. useRecuder
    const [data, dispatch] = useReducer(reducer, []);
    * data : state
     - 기존 useState에서 사용한 state를 그대로 사용
    * dispatch : 상태를 변화시키는 action을 발생시키는 함수
    * reducer : 상태 변화를 처리해주는 역할 (구현필요)
    * [] : 초기 state
    
    2. dispatch
    dispatch({ type: 'INIT', data: initData });
    * { type: 'INIT', .. } : action 객체 상태변화를 설명하는 객체
    
    3. reducer
    const reducer = (state, action) => {
      switch (action.type) {}
    }
    * state : 최신 state
    * action : dispach 함수에 전달한 action 객체

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import { useCallback, useMemo, useRef, useEffect, useState } from 'react';
+import { useCallback, useMemo, useRef, useEffect, useReducer } from 'react';
 import DiaryEditer from './DiaryEditer';
 import DiaryList from './DiaryList';
 import './App.scss';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE': {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date,
+      };
+      return [newItem, ...state];
+    }
+    case 'REMOVE': {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case 'EDIT': {
+      return state.map((it) =>
+        it.id === action.targetId ? { ...it, content: action.newContent } : it,
+      );
+    }
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
+
   const dataId = useRef(0);
 
   const getDate = async () => {
@@ -21,7 +48,7 @@ function App() {
       };
     });
 
-    setData(initData);
+    dispatch({ type: 'INIT', data: initData });
   };
 
   useEffect(() => {
@@ -30,30 +57,26 @@ function App() {
 
   // 등록
   const onCreate = useCallback((author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current,
-    };
+    dispatch({
+      type: 'CREATE',
+      data: {
+        author,
+        content,
+        emotion,
+        id: dataId.current,
+      },
+    });
     dataId.current += 1;
-    setData((data) => [newItem, ...data]);
   }, []);
 
   // 삭제
   const onRemove = useCallback((targetId) => {
-    setData((data) => data.filter((it) => it.id !== targetId));
+    dispatch({ type: 'REMOVE', targetId });
   }, []);
 
   // 수정
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) =>
-      data.map((it) =>
-        it.id === targetId ? { ...it, content: newContent } : it,
-      ),
-    );
+    dispatch({ type: 'EDIT', targetId, newContent });
   }, []);
 
   const getDiaryAnalysis = useMemo(() => {
